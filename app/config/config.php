4a49239eb59f40f267305be57baca931072bba53
Kevin6357 <1680504@senati.pe>
2026-05-02 14:13:18 -0500

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -36,4 +36,4 @@ define("DB_PORT", $_ENV['DB_PORT'] ?? '3306');
 define("DB_NAME", $_ENV['DB_DATABASE'] ?? '');
 define("DB_USER", $_ENV['DB_USERNAME'] ?? 'root');
 define("DB_PASS", $_ENV['DB_PASSWORD'] ?? '');
->>>>>>> 070e2238c035ab54a356cf3bb3d415df88624b3a
+
